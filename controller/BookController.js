d3e4d30a3b3f971e6e72665a5829d1fcf5eabd15
II-122 <76612998+II-122@users.noreply.github.com>
2024-05-26 19:39:52 +0900

    2024-05-26 commit1

--- controller/BookController.js
+++ controller/BookController.js
@@ -7,36 +7,45 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const checkBooks = (req, res) => {
-    let { categoryId } = req.query;
-    
-    if(categoryId) {
-        let sql = `SELECT * FROM books WHERE books_categoryId = ?`;
-        conn.query(sql, categoryId, (err, results) => {
-            if(err) {
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            
-            if(results.length) {
-                return res.status(StatusCodes.OK).json(results);
-            } else {
-                return res.status(StatusCodes.NOT_FOUND).end();
-            }
-        });
-    } else {
-        let sql = "SELECT * FROM books";
-        conn.query(sql, (err, results) => {
-            if(err) {
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            return res.status(StatusCodes.CREATED).json(results);
-        });
+    let { categoryId, newBook, limit , currentPage } = req.query;
+
+    // limit : Page 당 도서 수
+    // offset : 0, (currentPage-1) * limit
+    let offset = limit * (currentPage - 1);
+
+    let sql = `SELECT * FROM books`;
+    let values = [];
+    if(categoryId && newBook) {
+        sql += ` WHERE books_categoryId = ? 
+                AND books_pubDate BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+        values = values.push(categoryId);
+    } else if (categoryId) {
+        sql += ` WHERE books_categoryId = ?`;
+        values = values.push(categoryId);
+    } else if(newBook){
+        sql += ` WHERE books_pubDate BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
     }
-};
+    sql += ` LIMIT ? OFFSET ?`;
+    values = values.push(parseInt(limit), offset);
+    
+    conn.query(sql, values, (err, results) => {
+        if(err) {
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+            
+        if(results.length) {
+            return res.status(StatusCodes.OK).json(results);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+    });
+}; 
 
 const checkBook = (req, res) => {
     const { param_bookId } = req.params;
     
-    let sql = "SELECT * FROM books WHERE books_id = ?";
+    let sql = `SELECT * FROM books LEFT JOIN category 
+                ON books.books_categoryId = category.category_id WHERE books.books_id = ?;`;
     conn.query(sql, param_bookId, (err, results) => {
         if(err) {
             return res.status(StatusCodes.BAD_REQUEST).end();
